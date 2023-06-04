e46af2496bfd5dd953b1491924e4de97cfc1bce0
Otavius Whitehead <otaviuswhitehead1994@gmail.com>
2023-06-04 17:37:41 -0400

    update testing

--- test/conftest.py
+++ test/conftest.py
@@ -8,10 +8,10 @@ def test_new_user():
     Given a User model 
     It's going to test when a new user is created. 
     """
-    user = User("Nesia", "Whitehead", "nesiawhitehead@yahoo.", "flask", "nesia123")
-    assert user.first_name == "Nesia"
-    assert user.last_name == "Whitehead"
-    assert user.email == "nesiawhitehead@yahoo.com"
+    user = User("Chris", "Head", "chrishead@yahoo.", "flask", "head123")
+    assert user.first_name == "Chris"
+    assert user.last_name == "Head"
+    assert user.email == "chrishead@yahoo.com"
     assert user.password == "flask"
-    assert user.username == "nesia123"
+    assert user.username == "head123"
     
